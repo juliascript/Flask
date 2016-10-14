986cc1b5b2af8b353bf31a9335e523a23d0f14f7
Julia <juliageist23@gmail.com>
2016-10-14 14:58:23 -0700

    challenge 4 complete

--- testingFlask.py
+++ testingFlask.py
@@ -7,15 +7,18 @@ listOfPets = []
 def hello():
     return 'Hello there!\n'
 
-@app.route('/pets', methods=['POST'])
+@app.route('/pets', methods=['GET', 'POST'])
 def storePets():
-	result = request.args.to_dict()
-	print(result)
-	if ('name' in result.keys()) & ('age' in result.keys()) & ('species' in result.keys()):
-		listOfPets.append(result)
-		return json.dumps(result)
-	else:
-		return 'HTTP 400 Error: Bad Request -- Please provide name, age, and species in query string.'
+	if request.method == "GET":
+		return json.dumps(listOfPets)
+	elif request.method == "POST":
+		result = request.args.to_dict()
+		print(result)
+		if ('name' in result.keys()) & ('age' in result.keys()) & ('species' in result.keys()):
+			listOfPets.append(result)
+			return json.dumps(result)
+		else:
+			return 'HTTP 400 Error: Bad Request -- Please provide name, age, and species in query string.'
 
 
 if __name__ == '__main__':
