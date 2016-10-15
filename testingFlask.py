03dcf3a9db203f32f07832bb94da3d4e49dd7b94
Julia <juliageist23@gmail.com>
2016-10-14 22:36:28 -0700

    challenge 6 complete

--- testingFlask.py
+++ testingFlask.py
@@ -2,6 +2,7 @@ from flask import Flask, request, json
 app = Flask(__name__)
 
 listOfPets = []
+namesOfPets = []
 
 # / route
 @app.route('/')
@@ -26,23 +27,32 @@ def pets():
 		result = request.args.to_dict()
 		# If the required arguments (name, age, and species) are in the dictionary, proceed
 		if ('name' in result.keys()) & ('age' in result.keys()) & ('species' in result.keys()):
-			# Add the pet to the listOfPets list
-			listOfPets.append(result)
+			nameOfPet = result['name']
+			# If the name of new pet not already in the namesOfPets list, proceed
+			if nameOfPet not in namesOfPets:
+				# Add the pet to the listOfPets list
+				listOfPets.append(result)
+				# Add the name of the pet to the namesOfPets list (the name is currently being used as a unique id)
+				namesOfPets.append(nameOfPet)
+				return 'Pet added to store\n'
+			# If the name of the new pet is already in the namesOfPets list, return 409 error
+			else: 
+				return 'HTTP 409 Error: Conflict -- Pet already exists in store.\n', 409
 		# If the required arguments (name, age, and species) are not in the dictionary, return 400 error
 		else:
-			return 'HTTP 400 Error: Bad Request -- Please provide name, age, and species in query string.', 400
+			return 'HTTP 400 Error: Bad Request -- Please provide name, age, and species in query string.\n', 400
 
 # /pets/<name>
 @app.route('/pets/<path:name>')
 def petPath(name):
 	# Iterate over all pets stored in listOfPets
 	for pet in listOfPets:
-		# If the name of the pet is equal to the name specified in the path,
+		# If the name of the pet is equal to the name specified in the path, proceed
 		if pet['name'] == name:
 			# Return that pet
 			return json.dumps(pet)
 	# Otherwise, return a 404 error
-	return 'HTTP 404 Error: Page Not Found -- Pet does not exist in store', 404
+	return 'HTTP 404 Error: Page Not Found -- Pet does not exist in store\n', 404
 
 if __name__ == '__main__':
     app.run()
