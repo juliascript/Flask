fc4027372293e4d7d6636f862e46c38d94320300
Julia <juliageist23@gmail.com>
2016-10-14 23:46:13 -0700

    challenge 7 complete

--- testingFlask.py
+++ testingFlask.py
@@ -43,7 +43,7 @@ def pets():
 			return 'HTTP 400 Error: Bad Request -- Please provide name, age, and species in query string.\n', 400
 
 # /pets/<name>
-@app.route('/pets/<path:name>', methods=['GET', 'PUT'])
+@app.route('/pets/<path:name>', methods=['GET', 'PUT', 'DELETE'])
 def petPath(name):
 	if request.method == "GET":
 		# Iterate over all pets stored in listOfPets
@@ -54,23 +54,40 @@ def petPath(name):
 				return json.dumps(pet)
 		# Otherwise, return a 404 error
 		return 'HTTP 404 Error: Page Not Found -- Pet does not exist in store\n', 404
-	if request.method == "PUT":
+	elif request.method == "PUT":
 		# Parse the request arguments into a dictionary
 		result = request.args.to_dict()
 		# If the required arguments (only age, for now) are in the dictionary, proceed
 		if 'age' in result.keys():
 			# If the name of pet exists in the namesOfPets list, proceed
 			if name in namesOfPets:
-				# Determine the index of the specified pet in listOfPets based on its index in nameOfPets
+				# Determine the index of the specified pet in listOfPets based on its index in namesOfPets
 				indexOfPet = namesOfPets.index(name)
 				# Update all values in the pet object in the listOfPets dict 
 				# ** Add more values here to open up range of extension for PUT method ** 
 				listOfPets[indexOfPet]['age'] = result['age']
 				return ''
 			# If the name of the pet does not exist in the namesOfPets list, return 404 error
-			return 'HTTP 404 Error: Page Not Found -- Pet does not exist in store\n', 404
+			else:
+				return 'HTTP 404 Error: Page Not Found -- Pet does not exist in store\n', 404
 		# If the required arguments are not in the dictionary, return 400 error
-		return 'HTTP 400 Error: Bad Request -- Please age in query string to update values of pet.\n', 400
+		else:
+			return 'HTTP 400 Error: Bad Request -- Please age in query string to update values of pet.\n', 400
+	elif request.method == "DELETE":
+		# If the pet exists in namesOfPets, proceed
+		if name in namesOfPets:
+			# Determine the index of the specified pet in listOfPets based on its index in namesOfPets
+			indexOfPet = namesOfPets.index(name)
+			# Capture the pet object in a variable, to be return after deleting
+			petObject = listOfPets[indexOfPet]
+			# Delete the pet object in the listOfPets dict and namesOfPets list
+			del listOfPets[indexOfPet]
+			del namesOfPets[indexOfPet]
+			# Return the deleted pet object
+			return json.dumps(petObject)
+		# If the pet does not exist in namesOfPets, return 404 error 
+		else:
+			return 'HTTP 404 Error: Page Not Found -- Pet does not exist in store\n', 404
 
 
 
